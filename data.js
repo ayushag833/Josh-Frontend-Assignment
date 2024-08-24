59ac175e93b9a2d6080b203eaf93a247d5c6a209
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-08-24 21:15:29 +0530

    commit

--- data.js
+++ data.js
@@ -1,3 +1,60 @@
-let data = {
-  domain: "Front End Development",
-};
+let data = localStorage.getItem("data")
+  ? JSON.parse(localStorage.getItem("data"))
+  : [
+      {
+        domain: "Front End Development",
+        skill1: "Html",
+        skill2: "CSS",
+        skill3: "Js",
+        skill4: "PHP",
+        skill5: "WordPress",
+        number1: 90,
+        number2: 85,
+        number3: 80,
+        number4: 75,
+        number5: 85,
+      },
+      {
+        domain: "Languages",
+        skill1: "Hindi",
+        skill2: "English",
+        number1: 90,
+        number2: 85,
+      },
+      {
+        domain: "Back End Development",
+        skill1: "NodeJS",
+        skill2: "SSR",
+        number1: 90,
+        number2: 85,
+      },
+      {
+        domain: "Front End Development",
+        skill1: "Html",
+        skill2: "CSS",
+        skill3: "Js",
+        skill4: "PHP",
+        skill5: "WordPress",
+        number1: 90,
+        number2: 85,
+        number3: 80,
+        number4: 75,
+        number5: 85,
+      },
+      {
+        domain: "Languages",
+        skill1: "Hindi",
+        skill2: "English",
+        number1: 90,
+        number2: 85,
+      },
+      {
+        domain: "Back End Development",
+        skill1: "NodeJS",
+        skill2: "SSR",
+        number1: 90,
+        number2: 85,
+      },
+    ];
+
+export default data;
--- script.js
+++ script.js
@@ -1,63 +1,150 @@
-// $(".card-section1").slick({
-//   dots: true,
-//   infinite: true,
-//   arrows: false,
-//   speed: 300,
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   centerMode: true,
-//   centerPadding: "5%",
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         dots: true,
-//       },
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 1,
-//       },
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//       },
-//     },
-//   ],
-// });
+import data from "./data.js";
 
 const btn1 = document.querySelector(".add-skill-1");
-const btn2 = document.querySelector(".add-skill-2");
 const cancel = document.querySelector(".cancel");
 const modal = document.querySelector(".add-skill-modal");
 const links = document.querySelectorAll("nav ul a");
 const parentEle = document.querySelector("nav").parentElement;
 const skillCard = document.querySelector(".card-section-skills");
+const form = document.querySelector(".add-skill-modal form");
+const form1 = document.querySelector(".my-info form");
+const domain = document.getElementById("domain");
+const skill = document.getElementById("skill");
+const proficiency = document.getElementById("proficiency");
+const fullname = document.getElementById("fullname");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("msg");
+
+let h6 = document.createElement("h6");
+
+form1?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form1);
+  let obj = {};
+  let isValid = true;
+  formData.forEach((value, key) => {
+    if (isValid && key === "Full Name" && value === "") {
+      h6.textContent = "* Enter Full Name";
+      h6.classList.add("validation1");
+      fullname.insertAdjacentElement("afterend", h6);
+      isValid = false;
+    }
+    if (isValid && key === "Email" && value === "") {
+      h6.textContent = "* Enter Email";
+      h6.classList.add("validation1");
+      email.insertAdjacentElement("afterend", h6);
+      isValid = false;
+    }
+    if (isValid && key === "Subject" && value === "") {
+      h6.textContent = "* Enter Subject";
+      h6.classList.add("validation1");
+      subject.insertAdjacentElement("afterend", h6);
+      isValid = false;
+    }
+    if (isValid && key === "Message" && value === "") {
+      h6.textContent = "* Enter Message";
+      h6.classList.add("validation1");
+      message.insertAdjacentElement("afterend", h6);
+      isValid = false;
+    }
+    if (isValid && value) {
+      obj[key.trim()] = value.trim();
+    }
+  });
+  if (!isValid) {
+    window.scrollTo(2450, 2450);
+    return;
+  }
+  h6.remove();
+  console.log("Your Details :- ", obj);
+});
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  let obj = {};
+  let isValid = true;
+  formData.forEach((value, key) => {
+    if (isValid && key === "domain" && value === "") {
+      h6.textContent = "* Enter Domain";
+      h6.classList.add("validation");
+      domain.insertAdjacentElement("afterend", h6);
+      isValid = false;
+    }
+    if (isValid && key === "skill1" && value === "") {
+      h6.textContent = "* Enter atleast one skill";
+      h6.classList.add("validation");
+      skill.insertAdjacentElement("afterend", h6);
+      isValid = false;
+    }
+    if (isValid && key === "number1" && value === "") {
+      h6.textContent = "* Enter proficiency of first skill";
+      h6.classList.add("validation");
+      proficiency.insertAdjacentElement("afterend", h6);
+      isValid = false;
+    }
+    if (isValid && value) {
+      obj[key.trim()] = value.trim();
+    }
+  });
+  if (
+    isValid &&
+    ((obj.skill2 === undefined && obj.number2 !== undefined) ||
+      (obj.skill2 !== undefined && obj.number2 === undefined) ||
+      (obj.skill3 === undefined && obj.number3 !== undefined) ||
+      (obj.skill3 !== undefined && obj.number3 === undefined) ||
+      (obj.skill4 === undefined && obj.number4 !== undefined) ||
+      (obj.skill4 !== undefined && obj.number4 === undefined) ||
+      (obj.skill5 === undefined && obj.number5 !== undefined) ||
+      (obj.skill5 !== undefined && obj.number5 === undefined))
+  ) {
+    h6.textContent = "* Enter all details carefully";
+    h6.classList.add("validation");
+    skill.insertAdjacentElement("afterend", h6);
+    isValid = false;
+  }
+  if (
+    isValid &&
+    (obj.number1 < 0 ||
+      obj.number2 < 0 ||
+      obj.number3 < 0 ||
+      obj.number4 < 0 ||
+      obj.number5 < 0 ||
+      obj.number1 > 100 ||
+      obj.number2 > 100 ||
+      obj.number3 > 100 ||
+      obj.number4 > 100 ||
+      obj.number5 > 100)
+  ) {
+    h6.textContent = "* Proficiency value is invalid";
+    h6.classList.add("validation");
+    proficiency.insertAdjacentElement("afterend", h6);
+    isValid = false;
+  }
+  if (!isValid) {
+    window.scrollTo(0, 0);
+    return;
+  }
+  h6.remove();
+  data.push(obj);
+  localStorage.setItem("data", JSON.stringify(data));
+
+  modal.style.display = "none";
+  modal.parentElement.classList.remove("modal-overlay");
+  window.location.reload();
+});
 
 btn1?.addEventListener("click", () => {
   modal.style.display = "block";
   modal.parentElement.classList.add("modal-overlay");
   window.scrollTo(0, 0);
 });
-btn2?.addEventListener("click", (e) => {
-  e.preventDefault();
-  modal.style.display = "none";
-  modal.parentElement.classList.remove("modal-overlay");
-  window.scrollTo(0, 0);
-});
+
 cancel?.addEventListener("click", (e) => {
   e.preventDefault();
   modal.style.display = "none";
   modal.parentElement.classList.remove("modal-overlay");
-  window.scrollTo(0, 0);
 });
 
 let prevEle;
@@ -93,4 +180,62 @@ links.forEach((ele) => {
   });
 });
 
-skillCard.innerHTML += ``;
+if (skillCard)
+  data.forEach((ele) => {
+    skillCard.innerHTML += `<div class="card-skills">
+    <div class="card-content-skills">
+        <h3>${ele.domain}</h3>
+        <div class="taglines">
+            <div class="tagline">
+                <div>${ele.skill1}</div>
+                <div>${ele.number1}%</div>
+            </div>
+            <div class="line">
+                <div class="line-width" style = "width: ${
+                  ele.number1
+                }%";)}></div>
+            </div>
+            ${
+              ele.skill2
+                ? `<div class="tagline">
+                <div>${ele.skill2}</div>
+                <div>${ele.number2}%</div>
+            </div>
+            <div class="line">
+                <div class="line-width" style = "width: ${ele.number2}%";)}></div></div>`
+                : ``
+            }
+            ${
+              ele.skill3
+                ? `<div class="tagline">
+                    <div>${ele.skill3}</div>
+                    <div>${ele.number3}%</div>
+                </div>
+                <div class="line">
+                <div class="line-width" style = "width: ${ele.number3}%";)}></div></div>`
+                : ``
+            }
+            ${
+              ele.skill4
+                ? `<div class="tagline">
+                <div>${ele.skill4}</div>
+                <div>${ele.number4}%</div>
+            </div>
+            <div class="line">
+                <div class="line-width" style = "width: ${ele.number4}%";)}></div></div>`
+                : ``
+            }
+            ${
+              ele.skill5
+                ? `<div class="tagline">
+                <div>${ele.skill5}</div>
+                <div>${ele.number5}%</div>
+            </div>
+            <div class="line">
+                <div class="line-width" style = "width: ${ele.number5}%";)}></div></div>`
+                : ``
+            }
+        </div>
+    </div> 
+</div>`;
+  });
