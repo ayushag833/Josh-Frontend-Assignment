86a5100f77a2e7a2146da3cddc869697cade199c
ayushaggarwal600 <ayushaggarwal600@gmail.com>
2024-08-27 17:12:45 +0530

    Final Commit

--- script.js
+++ script.js
@@ -265,9 +265,8 @@ switch (window.location.hash) {
     prevLink = links[5];
     break;
 }
-console.log(window.location.pathname, window.location.hash);
 
-prevLink.classList.add("active");
+prevLink?.classList.add("active");
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
